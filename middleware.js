855a0fe9dd1b079043f6cf1d16ab020974c4229a
Angel050019 <angelgc.ti23@utsjr.edu.mx>
2024-08-30 19:37:10 -0600

    middlewares en archivo individual

--- middleware.js
+++ middleware.js
@@ -1,10 +1,21 @@
-var express = require('express');
-var app = express();
+var saludo = (req, res, next) => {
+    console.log("Hola");
+    next();    
+};
 
-var mid=((req,res,next)=>{
-    console.log("Middleware para todas las rutas");
-    next();
+var fecha = (req, res, next) => {
+    let hoy = new Date();
     
-});
-//exportamos la variable para mandarlo a routes
-module.exports = mid;
+    let dia = ("0" + hoy.getDate()).slice(-2);
+    let mes = ("0" + (hoy.getMonth() + 1)).slice(-2);
+    let anio = hoy.getFullYear();
+
+    let horas = ("0" + hoy.getHours()).slice(-2);
+    let minutos = ("0" + hoy.getMinutes()).slice(-2);
+    let segundos = ("0" + hoy.getSeconds()).slice(-2);
+
+    console.log(`${dia}/${mes}/${anio} ${horas}:${minutos}:${segundos}`);
+    next();    
+};
+
+module.exports = { saludo, fecha };
